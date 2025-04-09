fe793b42498497e09838fe8322904bba62551fff
p3n <puma3new@bk.ru>
2025-04-09 22:38:51 +0300

    + get_date(), преобразует datetime в d.m.y

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,22 @@
-import masks
+from datetime import datetime
+
+import masks  # type: ignore
+
 
 def mask_account_card(card_number: str) -> str:
     """Шифрует данные банковской карты или счёта при помощи masks.py"""
     # Разделяем текстовую часть и цифры
-    text = ''.join(filter(lambda x: x.isalpha() or x.isspace(), card_number))
-    digits = ''.join(filter(str.isdigit, card_number))
-    digits = int(digits)
-    
+    text = "".join(filter(lambda x: x.isalpha() or x.isspace(), card_number))
+    digits = "".join(filter(str.isdigit, card_number))
+
     # Проверяем наличие слов "Счёт" или "Счет" в текстовой части
     if "Счёт" in text or "Счет" in text:
-        return masks.get_mask_account(digits)
+        return str(masks.get_mask_account(int(digits)))
     else:
-        return masks.get_mask_card_number(digits)
+        return str(masks.get_mask_card_number(int(digits)))
+
+
+def get_date(timestamp: str) -> str:
+    """Преобразует дату из формата '2024-03-11T02:26:18.671407' в формат 'ДД.ММ.ГГГГ'."""
+    date_obj = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
